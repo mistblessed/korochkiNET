b8c3693da161ace2c54d81745f96356e67aaca88
Pashok <dadaf@gmail.com>
2026-04-18 15:08:33 +0300

    add DATABASE_URL

--- app/config.py
+++ app/config.py
@@ -4,13 +4,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DB_HOST = os.getenv('DB_HOST','localhost')
-    DB_PORT= os.getenv('DB_PORT','5432')
-    DB_USER = os.getenv('DB_USER','postgres')
-    DB_PASSWORD = os.getenv('DB_PASSWORD','5432')
-    DB_NAME = os.getenv('DB_NAME','korochki')
+    DATABASE_URL = os.environ.get('DATABASE_URL')
 
-    SECRET_KEY = 'fdsjfioegwifjef8w39rjwefioqwd3wr25refdg'
+    if DATABASE_URL is None:
+
+        DB_HOST = os.getenv('DB_HOST','localhost')
+        DB_PORT= os.getenv('DB_PORT','5432')
+        DB_USER = os.getenv('DB_USER','postgres')
+        DB_PASSWORD = os.getenv('DB_PASSWORD','5432')
+        DB_NAME = os.getenv('DB_NAME','korochki')
+
+    else:
+        pass
+
+    SECRET_KEY = os.getenv('SECRET_KEY', 'fdsjfioegwifjef8w39rjwefioqwd3wr25refdg')
 
     @staticmethod
     def get_db_uri():
--- app/db_utils.py
+++ app/db_utils.py
@@ -1,30 +1,33 @@
 import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg2.extras
 from config import Config
+import os
 
-def get_db_connection():
-    conn = psycopg2.connect(Config.get_db_uri())
-    return conn
+def get_connection():
+    if Config.DATABASE_URL:
+        # На Render используем DATABASE_URL
+        return psycopg2.connect(Config.DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor)
+    else:
+        # Локально используем отдельные параметры
+        return psycopg2.connect(
+            host=Config.DB_HOST,
+            dbname=Config.DB_NAME,
+            user=Config.DB_USER,
+            password=Config.DB_PASSWORD,
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
 
-def execute_query(query: str, params=None, fetch_one=False, fetch_all=False):
-    conn = get_db_connection()
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    try:
-        cur.execute(query, params)
-        if fetch_one:
-            result = cur.fetchone()
-        elif fetch_all:
-            result = cur.fetchall()
-        else:
-            result = None
+def execute_query(sql, params=None, fetch_all=False, fetch_one=False):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(sql, params)
+    if fetch_all:
+        result = cur.fetchall()
+    elif fetch_one:
+        result = cur.fetchone()
+    else:
+        result = None
         conn.commit()
-        return result
-    except Exception as e:
-        conn.rollback()
-        raise e
-    
-    finally:
-        cur.close()
-        conn.close()
-        
-
+    cur.close()
+    conn.close()
+    return result
