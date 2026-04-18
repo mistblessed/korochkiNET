688db78d613b6b273c83f3998446fdf9fbccfeb0
Pashok <dadaf@gmail.com>
2026-04-18 14:22:06 +0300

    add to Render

--- app/app.py
+++ app/app.py
@@ -488,5 +488,7 @@ def admin_update_status():
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
